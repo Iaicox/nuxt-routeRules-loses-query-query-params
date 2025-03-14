ae0fcb109fbe66c90c05eaebeeab3a78a4e2342a
Ilia Safin <mr.portulak@gmail.com>
2025-03-14 23:42:57 +0000

    added link only for client side

--- pages/test.vue
+++ pages/test.vue
@@ -4,9 +4,11 @@
     <NuxtLink to="/test?test=12345">Query params available</NuxtLink>
   </div>
   <div>
-    <NuxtLink to="/example?test=12345">
-      Query params disapear on redirect
-    </NuxtLink>
+    <client-only>
+      <NuxtLink to="/example?test=12345">
+        Query params disapear on redirect
+      </NuxtLink>
+    </client-only>
   </div>
   <pre>{{ useRoute().query }}</pre>
 </template>
