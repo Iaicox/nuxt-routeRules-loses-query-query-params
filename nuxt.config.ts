bbd04efde44a7deb39efdd26586a98db74cfbcd9
Ilia Safin <mr.portulak@gmail.com>
2025-03-14 23:17:42 +0000

    changed config generated static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-22',
   devtools: { enabled: true },
+  nitro: {
+    prerender: {
+      failOnError: false,
+      ignore: ['/example*'],
+    }
+  },
   routeRules: {
     '/example': { redirect: '/test' },
   },
