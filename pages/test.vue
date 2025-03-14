9a5524016ac927cf03a1bebc610823192f34b672
Ilia Safin <mr.portulak@gmail.com>
2025-03-14 15:23:34 +0000

    initial commit

---
+++ pages/test.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>This is Test page</div>
+  <div>
+    <NuxtLink to="/test?test=12345">Query params available</NuxtLink>
+  </div>
+  <div>
+    <NuxtLink to="/example?test=12345">
+      Query params disapear on redirect
+    </NuxtLink>
+  </div>
+  <pre>{{ useRoute().query }}</pre>
+</template>
