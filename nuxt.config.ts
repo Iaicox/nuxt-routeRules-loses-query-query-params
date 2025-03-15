3c6ed09b577815ac95418003b1ae5a8c04f86b74
Ilia Safin <mr.portulak@gmail.com>
2025-03-15 00:44:05 +0000

    config fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-22',
   devtools: { enabled: true },
+
+  ssr: false,
   app: {
-    baseURL: '/nuxt-routeRules-loses-query-query-params'
+    // baseURL: '/nuxt-routeRules-loses-query-query-params',
+    head: {
+      base: { href: '/nuxt-routeRules-loses-query-query-params' },
+    },
   },
   nitro: {
-    baseURL: '/nuxt-routeRules-loses-query-query-params',
+    // baseURL: '/nuxt-routeRules-loses-query-query-params',
     prerender: {
       failOnError: false,
       routes: [ '/' ],
