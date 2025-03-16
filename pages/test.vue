761c03770501a7d51deef53912bd6e1653b7fd56
Ilia Safin <mr.portulak@gmail.com>
2025-03-16 22:38:50 +0000

    minor fix

--- pages/test.vue
+++ pages/test.vue
@@ -1,11 +1,11 @@
 <template>
   <div>This is Test page</div>
   <div>
-    <NuxtLink to="/?test=12345">Query params available</NuxtLink>
+    <NuxtLink to="/test?test=12345">Query params available</NuxtLink>
   </div>
   <div>
     <client-only>
-      <NuxtLink to="/test?test=12345">
+      <NuxtLink to="/example?test=12345">
         Query params disapear on redirect
       </NuxtLink>
     </client-only>
